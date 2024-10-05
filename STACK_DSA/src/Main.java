503921e17ce27ee074348359f7565517503a414b
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2024-10-05 23:34:43 +0600

    Update 1

--- STACK_DSA/src/Main.java
+++ STACK_DSA/src/Main.java
@@ -1,7 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
+        // LIFO - Last in First out
+        // Push()
+        // Pull()
+        Stack<String> stack = new Stack<String>();
+        //
+
+        stack.push("Call of duty");
+        stack.push("Need for speed");
+        stack.push("GTA");
+        stack.push("Valorant");
+        stack.push("Final Fantacy");
+
+// check if stack is empty
+        System.out.println(stack.empty()); // output: false if not empty
+
+
+        // see what's inside the stack
+        System.out.println(stack);
+
+        // pop an item from the stack from top
+        stack.pop();
+        System.out.println("Item popped!\n");
+
+        System.out.println(stack);
+
+
+        String gamepooped = stack.pop();
+        System.out.println("Game popped: " + gamepooped);
+        System.out.println(stack);
+
+
+        // to see top most item
+        System.out.println("Top most item:\n");
+        System.out.println(stack.peek());
 
     }
+
+
+
+
 }
