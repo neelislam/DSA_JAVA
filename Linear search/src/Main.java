e6e4a86cbd03c299b137dd005df2dc757100d34a
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2024-11-03 23:57:16 +0600

    Update 8

--- Linear search/src/Main.java
+++ Linear search/src/Main.java
@@ -1,6 +1,26 @@
 
 public class Main {
     public static void main(String[] args) {
+         int[] array = {9,5,3,7,5,1,9,8,4,6,3,1,10};
+         int index  =  linearSearch(array, 10);
 
+         if(index!=-1){
+             System.out.println("Element found at index "+ index);
+
+         }
+         else{
+             System.out.println("Element not found");
+         }
+    }
+
+    private static int linearSearch(int[] array, int value){
+       for(int i = 0; i < array.length; i++){
+           if(array[i] == value){
+               return i;
+           }
+       }
+
+
+        return -1;
     }
 }
